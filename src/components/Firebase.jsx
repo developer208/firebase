f7ab299eeeedf7c4600f564fe8ffb2d2a986642e
developer208 <it.developer2002@gmail.com>
2023-01-18 15:36:18 +0530

    firebase Auth color --> yellow

--- src/components/Firebase.jsx
+++ src/components/Firebase.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Firebase = () => {
   return (
-    <div className='text-6xl text-white font-special  w-[100%] text-center my-5 md:text-9xl'>
-      Firebase Auth
-    </div>
-  )
-}
+    <>
+      <div className="text-6xl text-yellow-300 font-special  w-[100%] text-center mt-5 md:text-9xl">
+        Firebase Auth
+      </div>
+      <hr class="h-px mb-5 bg-yellow-300 w-[50%] mx-auto border-0 "></hr>
+    </>
+  );
+};
 
 export default Firebase;
